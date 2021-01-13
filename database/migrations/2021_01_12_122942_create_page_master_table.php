73ceda9cc19eec83b14488f14a776acb802a67c2
lenin <lenin@dreamguys.co.in>
2021-01-13 10:06:52 +0530

    language master initiate

---
+++ database/migrations/2021_01_12_122942_create_page_master_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePageMasterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_masters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_masters');
+    }
+}
