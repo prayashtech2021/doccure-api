0c3f4683bf8e67efd6c9dfe523628bb0e205b129
lenin <lenin@dreamguys.co.in>
2021-02-23 15:39:16 +0530

    patient doc list fix

--- app/Http/Controllers/Api/DoctorController.php
+++ app/Http/Controllers/Api/DoctorController.php
@@ -67,7 +67,7 @@ class DoctorController extends Controller
                 $patient_id = auth()->user()->id;
                 $list = User::role('doctor')->orderBy('created_at', $order_by);
 
-                $list = $list->whereHas('appointments', function ($qry) use ($doctor_id) {
+                $list = $list->whereHas('appointments', function ($qry) use ($patient_id) {
                     $qry->where('appointments.patient_id', $patient_id);
                 });
 
