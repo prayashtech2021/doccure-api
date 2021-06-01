f521215ec96c591b5783e8ac55c9c68b23321c6f
lenin <lenin@dreamguys.co.in>
2021-06-01 20:24:59 +0530

    test6 convertion utc

--- app/Helper/GeneralHelper.php
+++ app/Helper/GeneralHelper.php
@@ -59,8 +59,7 @@ function removeMetaColumn($model){
 function convertToUTC(Carbon $date, $timezone = null, $format = null)
 {
    if (!$timezone) $timezone = config('app.timezone');
-   $datetime = Carbon::createFromFormat('Y-m-d H:i:s', Carbon::now(), $timezone)->setTimezone(new DateTimeZone('UTC'));
-   print_r($timezone);
+   $datetime = Carbon::createFromFormat('Y-m-d H:i:s', $date->toDateTimeString(), $timezone)->setTimezone(new \DateTimeZone('UTC'));
    print_r($datetime);
    return $format ? $datetime->format($format) : $datetime;
 }
