22ca07ed1b14efc6c7e0f29014ea289473781581
saranya <saranya.g@dreamguys.co.in>
2021-06-21 15:01:48 +0530

    city seeder enabled

--- database/seeds/CitySeeder.php
+++ database/seeds/CitySeeder.php
@@ -16,11 +16,11 @@ class CitySeeder extends Seeder
             DB::table('cities')->delete();
             $json = File::get("database/data/cities.json");
             $data = json_decode($json, true);
-            // City::insert($data);
-            foreach($data as $item){
+            City::insert($data);
+            /*foreach($data as $item){
                 if($item['country_id']==101 && $item['state_id']==4035){
                     City::insert($item);
                 }
-            }
+            }*/
     }
 }
