e264dd277456a183bfbf2da899d017f745b8988a
lenin <lenin@dreamguys.co.in>
2021-06-30 12:33:16 +0530

    expire status fixed

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -94,11 +94,13 @@ class AppointmentController extends Controller
             foreach ($getApp as $item) {
                 $patient = User::find($item->user_id);
                 $patient_zone = Carbon::createFromFormat('Y-m-d H:i:s', Carbon::now()->toDateTimeString())->setTimezone($patient->time_zone);
-                if ($item->appointment_date < $patient_zone->format('Y-m-d H:i:s')) {
+                if ($item->appointment_date == $patient_zone->format('Y-m-d')) {
+                    if ($item->end_time < $patient_zone->format('H:i:s')) {
                     Appointment::where('id', $item->id)->update(['appointment_status' => 7]);
 
                     $requested_amount = $item->payment->total_amount - $item->payment->transaction_charge;
                     $patient->depositFloat($requested_amount);
+                    }
                 }
             }
 
