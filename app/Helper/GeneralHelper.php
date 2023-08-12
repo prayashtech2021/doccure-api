b9b23b7974a6d3c3ad2a104636665ecd83c12555
prayashtech2021 <prayashtech2021@gmail.com>
2023-08-12 09:55:50 +0800

    Profile Image Issue fixed

--- app/Helper/GeneralHelper.php
+++ app/Helper/GeneralHelper.php
@@ -17,7 +17,9 @@ function getUserProfileImage($user_id)
 {
     $user = User::find($user_id);
     if ($user && !empty($user->profile_image) && Storage::exists('images/profile-images/' . $user->profile_image)) {
-        return (config('filesystems.default') == 's3') ? Storage::temporaryUrl('images/profile-images/' . $user->profile_image, now()->addMinutes(5)) : url('storage/images/profile-images/' . $user->profile_image);
+        $profile_path =  env('APP_URL') . env('STORAGE_URL') . 'images/profile-images/' . $user->profile_image;
+        // return (config('filesystems.default') == 's3') ? Storage::temporaryUrl('images/profile-images/' . $user->profile_image, now()->addMinutes(5)) : url('storage/images/profile-images/' . $user->profile_image);
+        return (config('filesystems.default') == 's3') ? Storage::temporaryUrl('images/profile-images/' . $user->profile_image, now()->addMinutes(5)) : $profile_path;
 
     } else {
         return URL::asset('img/profile_image.jpg');
